1962082c92550e5d639fb7746074ed8c9040ecb2
Christian Torres <Christian.atm24@gmail.com>
2025-03-18 16:05:55 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import jwtAuth from "./routes/jwtAuth.js";
+
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(cors());
+
+// RUTAS
+// Ruta principal
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+//register and login routes
+app.use("/auth", jwtAuth);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
