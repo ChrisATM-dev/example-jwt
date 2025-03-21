da38ff9bc13782714d49a18cb9b13e21e7e52342
Christian Torres <Christian.atm24@gmail.com>
2025-03-21 13:34:22 -0300

    arreglo de variables

--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -9,16 +9,16 @@ import authorization from '../middleware/authorization.js';
 router.post("/register", async (req, res) => {
     try{
         //1. desestructure the req.body (name, email, password)
-        const { name, email, password } = req.body;
+        const { email, password } = req.body;
 
         //2. check if user exist (if user exist then throw error)
-        const user = await pool.query("Select * from users where user_email = $1",[email]);
+        const user = await pool.query("Select * from users where email = $1",[email]);
         
 
         //res.json(user.rows) se usa para ver lo que retorna ??????????
 
         if(user.rows.length !== 0){
-            return res.status(401).json("User already exist");
+            return res.status(401).json({error_msg: "User already exist"});
         }
 
         //3. bcrypt the user password
@@ -28,11 +28,11 @@ router.post("/register", async (req, res) => {
         const bcryptPassword = await bcrypt.hash(password, salt);
 
         //4. enter the new user inside our database
-        const newUser = await pool.query("INSERT INTO users (user_email, user_password, user_name) VALUES ($1, $2, $3) returning *", [email, bcryptPassword, name]);
+        const newUser = await pool.query("INSERT INTO users (email, password) VALUES ($1, $2) returning *", [email, bcryptPassword]);
         //res.status(201).send("Successfully registered user");, solo me muestra el mensaje pero no pueden haber mas de un status?
         
         //5. genrating our jwt token
-        const token = jwtGenerator(newUser.rows[0].user_id);
+        const token = jwtGenerator(newUser.rows[0].id);
 
         res.json({ token });
 
@@ -50,23 +50,23 @@ router.post("/login", async (req, res) => {
         const {email, password} = req.body;
 
         //2. check if user doesn't exist (if not then we throw error)
-        const user = await pool.query("select * from users where user_email = $1", [email]);
+        const user = await pool.query("select * from users where email = $1", [email]);
 
         if (user.rows.length === 0){
-            return res.status(401).json("Password or Email is incorrect");
+            return res.status(401).json({error_msg: "Password or Email is incorrect"});
 
         }
 
         //3. check if incomming password is the same the database passwonrd
-        const validPassword = await bcrypt.compare(password, user.rows[0].user_password);
+        const validPassword = await bcrypt.compare(password, user.rows[0].password);
 
         if(!validPassword){
-            return res.status(401).json("Password or Email is incorrect")
+            return res.status(401).json({error_msg: "Password or Email is incorrect"});
         }
  
         //4. give hen the jwt token
-        const token = jwtGenerator(user.rows[0].user_id);
-        const id = user.rows[0].user_id;
+        const token = jwtGenerator(user.rows[0].id);
+        const id = user.rows[0].id;
         res.json({ token , id});
 
 
@@ -79,13 +79,23 @@ router.post("/login", async (req, res) => {
 
 router.get("/is-verify", authorization, async (req, res) => {
     try {
-        const user = await pool.query("SELECT is_complete FROM users WHERE user_id = $1", [req.user]);
+        const user = await pool.query("SELECT is_complete FROM users WHERE id = $1", [req.user]);
         res.json({ verify: true, is_Complete: user.rows[0].is_complete });
     } catch (err) {
         console.error(err.message);
         res.status(500).send("Server Error");
     }
 });
+
+router.get("/get-users", async (req, res) => {
+    try {
+        const users = await pool.query("SELECT * FROM users");
+        res.json(users.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+})
 
 
 export default router;
